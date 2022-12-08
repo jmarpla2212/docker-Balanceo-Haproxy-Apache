0f84d472df5159a7175693000f190bc4f2ced83e
Juanchu <114174084+jmarpla2212@users.noreply.github.com>
2022-12-08 20:40:38 +0100

    Add files via upload

---
+++ apachito2/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+            $server_ip = $_SERVER["SERVER_ADDR"];
+            echo "<h1>Este es el Apache2 de Juanchu con IP ".$server_ip."</h1>";
+
+        ?>
+</html>
